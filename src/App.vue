0908763fb80d06a073a067b9b2d57a9314094e17
Abner <abnerodrigs@gmail.com>
2022-05-12 16:12:56 -0300

    chore: alterando tipo de autenticador

--- src/App.vue
+++ src/App.vue
@@ -25,68 +25,12 @@
     </button>
 
     <p>
-      {{ JSON.stringify(credential)}}
+      {{ JSON.stringify(credential) }}
     </p>
   </div>
 </template>
 
-<script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      username: '',
-      credential: null,
-    };
-  },
-  methods: {
-    async login() {
-      const publicKeyCredentialRequestOptions = {
-        challenge: Uint8Array.from('abner', (c) => c.charCodeAt(0)),
-        allowCredentials: [{
-          id: Uint8Array.from(this.credential, (c) => c.charCodeAt(0)),
-          type: 'public-key',
-        }],
-        timeout: 60000,
-      };
-
-      const assertion = await navigator.credentials.get({
-        publicKey: publicKeyCredentialRequestOptions,
-      });
-
-      console.log(assertion);
-    },
-    async registerWebAuthN() {
-      const publicKeyCredentialCreationOptions = {
-        challenge: Uint8Array.from('abner', (c) => c.charCodeAt(0)),
-        rp: {
-          name: 'Zoox WebAuthN',
-          // id: 'localhost',
-          id: 'webauthn-beta.vercel.app',
-          // id: "zooxdemo.rc.smartpass.com",
-        },
-        user: {
-          id: Uint8Array.from('UZSL85T9AFC', (c) => c.charCodeAt(0)),
-          name: this.username,
-          displayName: 'Abner Rodrigues',
-        },
-        pubKeyCredParams: [{
-          alg: -7,
-          type: 'public-key',
-        }],
-        timeout: 60000,
-        attestation: 'none',
-      };
-
-      const generatedCredentials = await navigator.credentials.create({
-        publicKey: publicKeyCredentialCreationOptions,
-      });
-
-      this.credential = generatedCredentials;
-    },
-  },
-};
-</script>
+<script src="./views/Home.js" />
 
 <style>
 * {
---
+++ src/views/Home.js
@@ -0,0 +1,56 @@
+export default {
+  name: 'App',
+  data() {
+    return {
+      username: '',
+      credential: null,
+    };
+  },
+  methods: {
+    async login() {
+      const publicKeyCredentialRequestOptions = {
+        challenge: Uint8Array.from('abner', (c) => c.charCodeAt(0)),
+        allowCredentials: [{
+          id: Uint8Array.from(this.credential, (c) => c.charCodeAt(0)),
+          type: 'public-key',
+        }],
+        userVerification: 'required',
+        timeout: 60000,
+      };
+
+      const assertion = await navigator.credentials.get({
+        publicKey: publicKeyCredentialRequestOptions,
+      });
+
+      // eslint-disable-next-line no-alert
+      alert(assertion);
+    },
+    async registerWebAuthN() {
+      const publicKeyCredentialCreationOptions = {
+        challenge: Uint8Array.from('abner', (c) => c.charCodeAt(0)),
+        rp: {
+          name: 'Zoox WebAuthN',
+          id: 'webauthn-beta.vercel.app',
+          // id: "zooxdemo.rc.smartpass.com",
+        },
+        user: {
+          id: Uint8Array.from('UZSL85T9AFC', (c) => c.charCodeAt(0)),
+          name: this.username,
+          displayName: 'Abner Rodrigues',
+        },
+        pubKeyCredParams: [{
+          alg: -7,
+          type: 'public-key',
+        }],
+        timeout: 60000,
+        attestation: 'none',
+      };
+
+      const generatedCredentials = await navigator.credentials.create({
+        publicKey: publicKeyCredentialCreationOptions,
+      });
+
+      this.credential = generatedCredentials;
+    },
+  },
+};
