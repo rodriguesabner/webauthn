d8a5cba73a02a1d86dca31db4f7a6ebdd06bd6ac
Abner <abnerodrigs@gmail.com>
2022-05-12 18:27:36 -0300

    chore: adiciona requireResidentKey

--- src/views/Home.js
+++ src/views/Home.js
@@ -41,6 +41,7 @@ export default {
         pubKeyCredParams: [{ alg: -7, type: 'public-key' }],
         authenticatorSelection: {
           authenticatorAttachment: 'platform',
+          requireResidentKey: false,
         },
         timeout: 60000,
         attestation: 'none',
