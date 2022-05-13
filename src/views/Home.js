1e568c7aa438677c2e86d5787639f543252a34d0
Abner <abnerodrigs@gmail.com>
2022-05-13 11:08:19 -0300

    chore: ajusta log

--- src/views/Home.js
+++ src/views/Home.js
@@ -39,7 +39,7 @@ export default {
     preformatMakeCredReq(makeCredReq) {
       const modieifiedCred = { ...makeCredReq };
       modieifiedCred.challenge = base64urlArraybuffer.decode(modieifiedCred.challenge);
-      alert(modieifiedCred.challenge);
+      alert(JSON.stringify(modieifiedCred));
       modieifiedCred.user.id = base64urlArraybuffer.decode(modieifiedCred.user.id);
 
       return modieifiedCred;
