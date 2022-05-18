484222d73450cfdfa0d6835ca0a00a21352bae83
Abner <abnerodrigs@gmail.com>
2022-05-18 11:40:47 -0300

    muda base64url

--- src/common/base64url-arraybuffer.js
+++ src/common/base64url-arraybuffer.js
@@ -1,4 +1,14 @@
 /* eslint-disable no-bitwise */
+
+/*
+ * Base64URL-ArrayBuffer
+ * https://github.com/herrjemand/Base64URL-ArrayBuffer
+ *
+ * Copyright (c) 2017 Yuriy Ackermann <ackermann.yuriy@gmail.com>
+ * Copyright (c) 2012 Niklas von Hertzen
+ * Licensed under the MIT license.
+ *
+ */
 const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789-_';
 
 // Use a lookup table to find the index.
@@ -7,31 +17,32 @@ for (let i = 0; i < chars.length; i += 1) {
   lookup[chars.charCodeAt(i)] = i;
 }
 
-const encode = (arraybuffer) => {
+// @ts-ignore
+function encode(arraybuffer) {
   const bytes = new Uint8Array(arraybuffer);
-  let i;
-  const len = bytes.length;
-  let base64url = '';
+  let i; const len = bytes.length; let
+    base64 = '';
 
   for (i = 0; i < len; i += 3) {
-    base64url += chars[bytes[i] >> 2];
-    base64url += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
-    base64url += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
-    base64url += chars[bytes[i + 2] & 63];
+    base64 += chars[bytes[i] >> 2];
+    base64 += chars[((bytes[i] & 3) << 4) | (bytes[i + 1] >> 4)];
+    base64 += chars[((bytes[i + 1] & 15) << 2) | (bytes[i + 2] >> 6)];
+    base64 += chars[bytes[i + 2] & 63];
   }
 
-  if (len % 3 === 2) {
-    base64url = base64url.substring(0, base64url.length - 1);
+  if ((len % 3) === 2) {
+    base64 = base64.substring(0, base64.length - 1);
   } else if (len % 3 === 1) {
-    base64url = base64url.substring(0, base64url.length - 2);
+    base64 = base64.substring(0, base64.length - 2);
   }
 
-  return base64url;
-};
+  return base64;
+}
 
-const decode = (base64string) => {
-  const bufferLength = base64string.length * 0.75;
-  const len = base64string.length;
+// @ts-ignore
+function decode(base64) {
+  const bufferLength = base64.length * 0.75;
+  const len = base64.length;
   let i;
   let p = 0;
   let encoded1;
@@ -39,21 +50,22 @@ const decode = (base64string) => {
   let encoded3;
   let encoded4;
 
-  const bytes = new Uint8Array(bufferLength);
+  const arraybuffer = new ArrayBuffer(bufferLength);
+  const bytes = new Uint8Array(arraybuffer);
 
   for (i = 0; i < len; i += 4) {
-    encoded1 = lookup[base64string.charCodeAt(i)];
-    encoded2 = lookup[base64string.charCodeAt(i + 1)];
-    encoded3 = lookup[base64string.charCodeAt(i + 2)];
-    encoded4 = lookup[base64string.charCodeAt(i + 3)];
+    encoded1 = lookup[base64.charCodeAt(i)];
+    encoded2 = lookup[base64.charCodeAt(i + 1)];
+    encoded3 = lookup[base64.charCodeAt(i + 2)];
+    encoded4 = lookup[base64.charCodeAt(i + 3)];
 
     bytes[p += 1] = (encoded1 << 2) | (encoded2 >> 4);
     bytes[p += 1] = ((encoded2 & 15) << 4) | (encoded3 >> 2);
     bytes[p += 1] = ((encoded3 & 3) << 6) | (encoded4 & 63);
   }
 
-  return bytes.buffer;
-};
+  return arraybuffer;
+}
 
 const methods = {
   decode,
